197116ecae0c4935de129a734953cb8d8c550cad
Fergus Coulter <41594162+FergusCoulter@users.noreply.github.com>
2022-08-14 20:07:55 +0100

    Final bits and bobs

--- TechnicalInterview/Program.cs
+++ TechnicalInterview/Program.cs
@@ -10,12 +10,17 @@ foreach(var card in deck.Deck)
 }
 Console.ReadLine();
 
+
+
 deck.Shuffle();
 foreach (var card in deck.Deck)
 {
     Console.WriteLine(card.ToString());
 }
 Console.ReadLine();
+
+
+
 
 deck.Sort(SortOrdinals.VALUE);
 foreach (var card in deck.Deck)
